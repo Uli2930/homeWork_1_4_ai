3ddddd2b4a0e43a556d3be0da6bca2f23bb7d3be
Uli2930 <uli2930tong@gmail.com>
2024-03-10 20:22:18 +0300

    gvgdvgvcegvceyvc

--- app/src/main/java/com/geeks/homework_1_4_ai/Keys.kt
+++
@@ -1,5 +0,0 @@
-package com.geeks.homework_1_4_ai
-
-object Keys {
-const val KEY_FOR_EMAIL = "key_for_email"
-}
--- app/src/main/java/com/geeks/homework_1_4_ai/fragments/MainFragment.kt
+++ app/src/main/java/com/geeks/homework_1_4_ai/fragments/MainFragment.kt
@@ -5,13 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.os.bundleOf
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.fragment.navArgs
-import com.geeks.homework_1_4_ai.Email
-import com.geeks.homework_1_4_ai.Keys
-import com.geeks.homework_1_4_ai.R
-import com.geeks.homework_1_4_ai.databinding.ActivityMainBinding
 import com.geeks.homework_1_4_ai.databinding.FragmentMainBinding
 
 
